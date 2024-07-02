6759d7c6e8209da3db0f6cc7417e54dd9e5521d4
federico Lubbert <federico.lubbert@gmail.com>
2024-07-02 08:33:43 +0200

    added

---
+++ src/game/camera.py
@@ -0,0 +1,23 @@
+import pygame
+from src.utils.constants import WINDOW_WIDTH, WINDOW_HEIGHT
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pygame.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        return entity.rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.centerx + int(WINDOW_WIDTH / 2)
+        y = -target.rect.centery + int(WINDOW_HEIGHT / 2)
+
+        # Limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - WINDOW_WIDTH), x)  # right
+        y = max(-(self.height - WINDOW_HEIGHT), y)  # bottom
+
+        self.camera = pygame.Rect(x, y, self.width, self.height)
